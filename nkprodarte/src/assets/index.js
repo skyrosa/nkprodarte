75cdfba87c043793fcde32fe0c2723e785706115
Scarlet Rosa <likedyourbody@gmail.com>
2023-07-07 13:55:39 +0200

    config, tailwind install, folders, imgs

---
+++ nkprodarte/src/assets/index.js
@@ -0,0 +1,16 @@
+import blog from "./blog.png"
+import blog1 from "./blog.png"
+import cart from "./cart.png"
+import cart1 from "./cart1.png"
+import facebook from "./facebook.png"
+import facebook1 from "./facebook1.png"
+import instagram from "./instagram.png"
+import instagram1 from "./instagram1.png"
+import twitter from "./twitter.png"
+import twitter1 from "./twitter1.png"
+import youtube from "./youtube.png"
+import youtube1 from "./youtube1.png"
+import logoblack from "./logoblack.png"
+import logowhite from "./logowhite.png"
+
+export { blog, blog1, cart, cart1, facebook, facebook1, twitter, twitter1, youtube, youtube1, instagram, instagram1, logowhite, logoblack }
